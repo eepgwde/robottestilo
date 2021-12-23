164e6ce0fce693ea224e5b8a5f03b31e42c166b4
Walter Eaves <walti@walti.mooo.com>
2021-12-23 17:07:33 +0100

    noted

--- README.md
+++ README.md
@@ -2,6 +2,258 @@
 
 weaves
 
+# Android and Appium Command-Line Tools
+
+These utilities work for Linux and Cygwin on Windows. If anyone manages to get it to work
+on MacOS do let me know. I would like to know If anyone manages to extend this to support
+iOS apps.
+
+## Installing 
+
+These scripts need to be installed in an Android SDK directory. If you use Android Studio
+to do that, then find your SDK directory and checkout the code from GitHub to here using
+this sequence:
+
+    git init
+    git remote add origin PATH/TO/REPO
+    git fetch
+    # Required when the versioned files existed in path before "git init" of this repo.
+    git reset origin/master  
+    git checkout -t origin/master
+
+And you should get this file on your local disk.
+
+I also try and simplify my Android configuration with some links. Most important is that
+my .android directory is here and not in my home directory and I have a link to expose it
+here.
+
+    build-tools/32.0.0-rc1/ ./btools
+    .android ./android
+    etc/hlpr ./hlpr
+    
+There is also a local copy of my script wrapper m_ here etc/hlpr and that is linked to ./hlpr - it should also
+be executable. I hope it works, it is used a lot.
+
+A quick word about it: hlpr is a wrapper that loads all the shell functions in hlpr.sh. It can also use hlpr.def
+for constants. The script hlpr has lots of switches.
+
+When debugging, use it like this: hlpr -n -x
+
+## What these Shell Scripts, XSLT and GNU Make Files Do
+
+### Managing emulator start-up
+
+There is a Makefile emu.mk that can start and stop
+
+ - the Android Emulator
+ - the Appium server using Node
+
+### Starting and Stopping App Sessions ready for Cucumber and Gherkin
+
+If you create a JSON file with your Android package's parameters
+
+Then the script can send it to the Appium server and your Android App should start.
+
+### Interrogating your App using Appium
+
+You can also do a few simple things with the script. You can get
+
+ - The session identifier
+
+ - The current page on the app
+
+It can get the session identifier, this is useful if you have connected using another
+system.
+
+Most usefully, you can get the XML of the current page on display.
+
+### XSLT post-processing of XML files
+
+If you store your XML pages to the pages/ directory, you can use the other GNU Make
+utility, xsl0.mk, to produce XPath and Java properties files of the pages.
+
+# Emulator Control and Appium Sessions
+
+If you are using the SDK, you will need to set up your environment to use the Android
+command-line utilities. I have an example configuration for Cygwin in
+etc/android0-cygwin. It uses this utility to manage paths: etc/pathmerge.
+
+## Starting and Stopping the Emulator
+
+Starting up: try this no-execute incantation of GNU Make: make -B -n all-local. It shows
+you what will be done. 
+
+    make -B -n all-local
+    echo OSTYPE - Cygwin
+    echo $$
+    test -n "L:\cache\Android\sdk"
+    test -d "L:\cache\Android\sdk"
+    adb start-server
+    nohup appium -g appium-$$.log --log-timestamp --local-timezone --log-no-colors --tmp /var/tmp --suppress-adb-kill-server > /dev/null 2>&1 &
+    touch adb-server.flag
+    nohup emulator/emulator @emulator2   > emulator.log 2>&1 &
+
+And to see what is done to stop the test system, try make -B -n clean-local
+
+You can eventually try this sequence repeatedly until you see success
+
+    make -B all-local
+    # check logs, correct as needed and then
+    make -B clean-local
+
+You can check the Appium log with hlpr mk log.
+
+When the Applium log looks good, it will probably work reliably, so you can side-step all
+the trace statements and do this in one operation:
+
+    hlpr mk cold
+
+This also performs a start from cold - a full restart for the emulator.
+
+## Starting Sessions
+
+This is a simple Curl operation. You do need a well-configured JSON file. There are some
+samples in the directory etc/appium. Using the -n switch to hlpr you can see what it will do.
+
+    $ hlpr -n app session-create
+    curl -s --output /var/tmp/tmp.l50oSDNRxV -H Content-type: application/json -X POST \
+     http://localhost:4723/wd/hub/session -d @etc/appium/session-create-cygwin.json
+
+The script has some logic to switch the file used by operating system. You can override
+on the command-line with the -f switch before, for example:
+ 
+    $ hlpr -n -f ./my-session-create.json app session-create
+    curl -s --output /var/tmp/tmp.Jb3AZ8cPox -H Content-type: application/json -X POST \
+     http://localhost:4723/wd/hub/session -d @./my-session-create.json
+
+If the JSON is well-formed, you should see your App come to life. If not, check the
+appium-*.log file with
+
+    $ hlpr mk log
+
+and you will discover that a path is wrong or you have noReset and fullReset both true.
+
+When you do get the system running, you can also try the Makefile version
+
+ make session-live
+
+this checks there is an existing session and if not starts a new one. (This is useful for
+automated testing, if you want to test with newCommandTimeout set to something non-zero.)
+
+### Recommendations
+
+Usually, I use this to capture XML files of the screens on display in the App. So I like
+to use fullReset for a clean startup, but I have to login every time.
+
+I usually set newCommandTimeout to 0 so that I can interact with the App and snapshot when
+the page changes.
+
+You can also run Appium Inspector with this. The Inspector has an "Attach to Session" feature.
+
+## Snapshotting Screens
+
+One of the things that should happen when you use hlpr app session-create is a file
+containing the session identifier is created. The file is session.json-id. (The whole
+response is in the file session.json - the capabilities are given in there.)
+
+If that session.json-id file exists, then this
+
+    $ hlpr app page
+
+will capture the page displayed as Selenium XML. There is more sophisticated version of
+this invocation that will write to the pages/ directory using temporary file names. To be
+compatible with the XSLT processing, the files are prefixed with "w" and have no suffix.
+
+    $ hlpr app page-pages
+
+is the invocation to add another page capture to the pages/ directory. This also has a
+comments feature. A comment file will be appended to. Comment files are in the pages
+directory and should start with "c". You can put a comment on the command-line
+
+    $ hlpr -l pages/c-locked1 app page-pages consent page
+
+It calculates a checksum and puts that into the comments file with a timestamp, you can
+put this into a shell script to snapshot every minute if you wish:
+
+    $ nohup bash -c 'while sleep 60; do hlpr -l pages/c-locked1 app page-pages ; done' &
+
+## Converting testconfig.properties files
+
+Some Appium implementations using their java-client put their test configuration into a
+properties file. You can convert to a JSON file using this script. Using my sample
+testconfig.properties in the etc/appium directory, that looks like this.
+
+   $ hlpr -l my-session-create.json conv appium etc/appium/testconfig.properties
+
+# XSLT Post-Processing
+
+To use these operations, you will need xmlstarlet and xsltproc.
+
+## Checking XPaths
+
+You use 'hlpr xml' to post-process individual files with xmlstarlet. This is for checking
+XPath.
+
+By default 'hlpr xml' will use the most recent file in the pages/ directory by
+default. You should try and remember what screen appeared for this file or use the
+comments feature when capturing.
+
+You can try out some XPaths on the captured files in pages/. Use the examples in the d_xml
+function of hlpr.sh for guidelines. 
+
+### General Structural Enquiries
+
+#### Structure
+
+You can get a sense of the depth and verbosity of the XML files with this
+
+ hlpr xml structure
+
+It just lists the elements.
+
+Similarly, you can see the last file's interesting elements:
+
+ hlpr xml textview # TextView
+ hlpr xml clickable-radiobutton # RadioButton
+ hlpr xml clickable-edittext # EditText
+
+And general enquiries:
+
+ hlpr xml text-nonempty # anything with text
+ hlpr xml clickable # any clickable fields
+
+#### XPath Attributes and Text Elements
+
+##### Attributes
+
+This can be useful
+
+    hlpr xml attnames
+
+The gets all the attributes names in order. It's a good idea to sort uniquely with
+
+ hlpr xml attnames | sort -u
+
+and you should see the attributes used on all the elements. This method is useful for
+checking if you have a non-compliant file.
+
+##### Text Elements
+
+You can get all interesting elements in a Properties file format. 
+
+  hlpr xml text
+
+The file will be large. 10000 lines is typical. You can 
+
+
+##### XPaths
+
+You can get all the XPaths for every attribute from the file.
+
+  hlpr xml xpath
+
+The file will be large. 10000 lines is typical. You can 
+
 # Android Development on Windows and Linux #
 
 Windows can run the emulator more quickly than Linux in nested virtualization.
--- hlpr.sh
+++ hlpr.sh
@@ -1,8 +1,37 @@
 # weaves
-# This script file uses m_ from
+# This script uses a wrapper file called m_ from
 # https://sourceforge.net/p/ill/site/base/ci/master/tree/bin/m_
-#
+# There is a local copy.
 # It has a function f_tpush to delete temporary files on exit.
+
+h_mr () {
+    cat >&2 <<EOF
+    Most recent file
+
+    $prog [-d dir] ${FUNCNAME##h_} 
+    $prog [-d dir] ${FUNCNAME##h_} 'appium-*.log'
+
+    The default directory is . 
+
+    The first form looks at all files in the directory and returns the name of the most recent.
+
+    The second form uses a hard-quoted glob specification like 'appium-*.log'
+    It then finds the most recent file to match that spec in the directory.
+EOF
+
+}
+
+d_mr () {
+    : ${d_dir:=.}
+
+    if [ $# -ge 1 ]
+    then
+        find -L "$d_dir" -maxdepth 1 -type f -name "$*" -printf "%T@ " -print | sort -rnk1 | head -1 | cut -d' ' --complement -f1 
+    else
+        ls -t "$d_dir" | head -1 | sed 's,^,'""${d_dir}""'/,g'
+    fi
+
+}
 
 h_applog () {
     cat >&2 <<EOF
@@ -31,56 +60,56 @@ EOF
 }
 
 d_json_pp () {
-  # pretty print JSON
-  $nodo python3 -m json.tool
+    # pretty print JSON
+    $nodo python3 -m json.tool
 }
 
 d_applog () {
-  test $# -ge 1 || return 1
-  local cmd="$1"
-  shift
+    test $# -ge 1 || return 1
+    local cmd="$1"
+    shift
 
-  : ${d_dir:=$PWD}
-  : ${d_file:=$(m_ -d $PWD mr 'appium-*.log')}
-  : ${d_log:=$(echo $d_file | sed 's/appium/adb/g')}
+    : ${d_dir:=$PWD}
+    : ${d_file:=$($prog -d $PWD mr 'appium-*.log')}
+    : ${d_log:=$(echo $d_file | sed 's/appium/adb/g')}
 
-  case $cmd in
-    session-create)
-      awk 'BEGIN { rec0= 0 }
+    case $cmd in
+	session-create)
+	    awk 'BEGIN { rec0= 0 }
 rec0 > 0 { $1=""; $2=""; $3=""; sess0=$0 ; rec0= 0; next }
 $3 ~ /^\[HTTP\]/ && $4 ~ /-->/ && $5 ~ /POST/ && $6 ~ /\/wd\/hub\/session$/ {
  rec0=NR+1; next
 }
 END { sub(/^[ ]+/, "", sess0); print sess0 }
 ' $d_file | e_json_pp > ${cmd}.json
-      ;;
+	    ;;
 
-    session-id)
-      test $# -ge 1 || return 2
-      test -f "$1" || return 4
-      jq '.value.sessionId' < $1 | tr -d \" 
-      ;;
+	session-id)
+	    test $# -ge 1 || return 2
+	    test -f "$1" || return 4
+	    jq '.value.sessionId' < $1 | tr -d \" 
+	    ;;
 
-    session-*)
-      local tag=${cmd##session-}
-      awk -v tag="[${tag}]" 'BEGIN { print tag } $3 == tag { print }' $d_file 
-      ;;
+	session-*)
+	    local tag=${cmd##session-}
+	    awk -v tag="[${tag}]" 'BEGIN { print tag } $3 == tag { print }' $d_file 
+	    ;;
 
-    cmd)
-      sed -e 's/shell \([^'\'']\)\(.*\)/shell '\''\1\2\'\''/g' 
-      ;;
-    log)
-      egrep 'Running ' $d_file | sed 's/^.* Running //g' | \
-        sed -e s/^\'//g -e s/\'$//g -e 's/^.*\/adb /adb /g' | $FUNCNAME cmd > $d_log
-      ;;
-	  view)
-      echo $1 $d_dir $d_file
-      ;;
-  esac
+	cmd)
+	    sed -e 's/shell \([^'\'']\)\(.*\)/shell '\''\1\2\'\''/g' 
+	    ;;
+	log)
+	    egrep 'Running ' $d_file | sed 's/^.* Running //g' | \
+		sed -e s/^\'//g -e s/\'$//g -e 's/^.*\/adb /adb /g' | $FUNCNAME cmd > $d_log
+	    ;;
+	view)
+	    echo $1 $d_dir $d_file
+	    ;;
+    esac
 }
 
 h_app () {
-  cat >&2 <<EOF
+    cat >&2 <<EOF
 
     Send commands to the app
 
@@ -106,92 +135,125 @@ EOF
 }
 
 d_app () {
-  test $# -ge 1 || return 1
-  local cmd="$1"
-  shift
+    test $# -ge 1 || return 1
+    local cmd="$1"
+    shift
 
-  : ${d_dir:=$PWD}
+    case $cmd in
+	cmd0)
+	    ## Use a named file (usually temporary) as output
+	    test -n "$e_file" || return 2
+	    $nodo curl -s --output $e_file -H 'Content-type: application/json' $*
+	    ;;
 
-  local tfile=$(mktemp)
-  f_tpush $tfile
+	cmd1)
+	    $nodo curl -s -H 'Content-type: application/json' $* 
+	    ;;
 
-  case $cmd in
-    cmd0)
-      ## Use a named file (usually temporary) as output
-      test -n "$e_file" || return 2
-      $nodo curl -s --output $e_file -H 'Content-type: application/json' $*
-      ;;
+	cmd2)
+	    : ${d_dir:=$PWD}
+	    : ${d_service:=http://127.0.0.1:4723/wd/hub}
 
-    cmd1)
-      $nodo curl -s -H 'Content-type: application/json' $* 
-      ;;
+	    test -f ${d_dir}/session.json-id || return 2
+	    local sid
+	    local cmd
+	    read sid < <(cat ${d_dir}/session.json-id)
+	    echo $sid > $verbose
 
-    cmd2)
-	: ${d_service:=http://127.0.0.1:4723/wd/hub}
+	    read cmd < <(echo "$@" | sed -e 's,:session_id/,'$sid/',g')
+	    
+	    $nodo curl -s "${d_service}${cmd}"
+	    ;;
 
-	test -f ${d_dir}/session.json-id || return 2
-	local sid
-	local cmd
-	read sid < <(cat ${d_dir}/session.json-id)
-	echo $sid > $verbose
+	session-isnull)
+	    read sid < <($FUNCNAME session-list | jq -r '.value[0]')
+	    test -n "$sid" || return 2
+	    if [ "$sid" = "null" ]
+	    then
+		return 0
+	    fi
+	    return 1
+	    ;;
 
-	read cmd < <(echo "$@" | sed -e 's,:session_id/,'$sid/',g')
-	
-      $nodo curl -s "${d_service}${cmd}"
-      ;;
+	_pagefile)
+	    # On cygwin, it doesn't work as described.
+	    TMPDIR=${d_dir} mktemp --tmpdir=${d_dir} -t wXXXXXX
+	    ;;
 
-    session-isnull)
-      read sid < <($FUNCNAME session-list | jq -r '.value[0]')
-      test -n "$sid" || return 2
-      if [ "$sid" = "null" ]
-      then
-        return 0
-      fi
-      return 1
-      ;;
+	_commentfile)
+	    if [ -n "$d_log" ]
+	    then
+		test -w "$d_log" || return 4
+		echo $d_log
+	    else
+		# On cygwin, mktemp doesn't work 
+		read mr < <($prog -d ${d_dir} mr 'c*')
+		if [ -z "$mr" ]
+		then
+		    TMPDIR=${d_dir} mktemp --tmpdir=${d_dir} -t cXXXXXX
+		else
+		    echo $mr
+		fi
+	    fi
+	    ;;
 
-    page) # it's JSON from the server.
-	$FUNCNAME cmd2 /session/:session_id/source | jq -r '.value'
-	;;
+	page-pages)
+	    : ${d_dir:=$PWD/pages}
+	    test -d "$d_dir" || mkdir -p ${d_dir}
+	    local tfile=$($FUNCNAME _pagefile)
+	    local cfile=$($FUNCNAME _commentfile)
+	    unset d_dir
+	    $FUNCNAME page > $tfile
+	    ## and write a comment
+	    echo $(date --iso-8601=minutes) : $tfile : $(sum $tfile) : "$*" >> $cfile
+	    ;;
 
-    session-list-id)
-      $FUNCNAME session-list | d_json_pp | jq -r '.value[0].id'
-      ;;
+	page) # it's JSON from the server.
+	    $FUNCNAME cmd2 /session/:session_id/source | jq -r '.value'
+	    ;;
 
-    session-list)
-      $FUNCNAME cmd1 -X GET http://localhost:4723/wd/hub/sessions
-      # > $tfile
-      # cat $tfile | jq -r '.value[0].id'
-      ;;
+	session-list-id)
+	    $FUNCNAME session-list | d_json_pp | jq -r '.value[0].id'
+	    ;;
 
-    session-id)
-      test $# -ge 1 || return 2
-      test -f $1 || return 2
-      d_applog session-id $1
-      ;;
+	session-list)
+	    $FUNCNAME cmd1 -X GET http://localhost:4723/wd/hub/sessions
+	    # > $tfile
+	    # cat $tfile | jq -r '.value[0].id'
+	    ;;
 
-    session-create)
-	# use applog session-id
-	: ${d_file:=etc/appium/${cmd}-${OSTYPE}.json}
-	: ${d_file:=etc/appium/${cmd}.json}
+	session-id)
+	    test $# -ge 1 || return 2
+	    test -f $1 || return 2
+	    d_applog session-id $1
+	    ;;
 
-      test -f ${d_file} || return 2
-      : ${d_log:=${d_dir}/session.json}
+	session-create)
+	    # use applog session-id
+	    : ${d_dir:=$PWD}
 
-      # Fix the temporary file
-      e_file=$tfile
-      $FUNCNAME cmd0 -X POST http://localhost:4723/wd/hub/session -d @${d_file}
-      sync
-      cp -b $e_file session-create.out
-      test -s $e_file || return 2
-      cat $e_file | d_json_pp > $d_log
-      $FUNCNAME session-id $d_log > ${d_log}-id
-     ;;
-  esac
+	    : ${d_file:=etc/appium/${cmd}-${OSTYPE}.json}
+	    : ${d_file:=etc/appium/${cmd}.json}
+
+	    test -f ${d_file} || return 2
+	    : ${d_log:=${d_dir}/session.json}
+
+	    # Setup a temporary file, deleted on exit
+	    e_file=$(mktemp)
+	    f_tpush $tfile
+
+	    $FUNCNAME cmd0 -X POST http://localhost:4723/wd/hub/session -d @${d_file}
+
+	    cp -b $e_file session-create.out
+	    test -s $e_file || return 2
+	    cat $e_file | d_json_pp > $d_log
+	    $FUNCNAME session-id $d_log > ${d_log}-id
+	    ;;
+    esac
 }
 
 h_adb () {
-  cat >&2 <<EOF
+    cat >&2 <<EOF
 
     $prog ${FUNCNAME##h_} cmd|snippet|trim
 
@@ -222,76 +284,96 @@ EOF
 }
 
 e_null () {
-  cat
+    cat
 }
 
 e_trim () {
-  cut -c30- 
+    cut -c30- 
 }
 
 d_adb () {
-  test $# -ge 1 || return 1
-  local cmd="$1"
-  shift
+    test $# -ge 1 || return 1
+    local cmd="$1"
+    shift
 
-  e_weaves="e_trim"
-  e_user=e_${d_user}
-  : ${d_machine:=emulator-5554}
+    e_weaves="e_trim"
+    e_user=e_${d_user}
+    : ${d_machine:=emulator-5554}
 
-  # : ${d_service:=adb -P 5037 -s ${d_machine}}
-  : ${d_service:=adb -P 5037}
-  : ${d_log:=adb0.sh}
+    # : ${d_service:=adb -P 5037 -s ${d_machine}}
+    : ${d_service:=adb -P 5037}
+    : ${d_log:=adb0.sh}
 
-  case $cmd in
-    trim)
-      $e_user
-      ;;
+    case $cmd in
+	trim)
+	    $e_user
+	    ;;
 
-    run)
-      $nodo bash $d_log
-      ;;
+	run)
+	    $nodo bash $d_log
+	    ;;
 
-    settings)
-      $d_service shell am start -n com.android.settings/.Settings
-      ;;
+	settings)
+	    $d_service shell am start -n com.android.settings/.Settings
+	    ;;
 
-    script)
-      cat $* | while read line0; do $FUNCNAME cmd $line0; done > $d_log
-      ;;
-    snippet)
-      $e_user | while read line0; do $FUNCNAME cmd $line0; done > $d_log
-      ;;
-    cmd)
-      echo $d_service $*
-      ;;
-  esac
+	script)
+	    cat $* | while read line0; do $FUNCNAME cmd $line0; done > $d_log
+	    ;;
+	snippet)
+	    $e_user | while read line0; do $FUNCNAME cmd $line0; done > $d_log
+	    ;;
+	cmd)
+	    echo $d_service $*
+	    ;;
+    esac
+}
+
+h_mk () {
+    cat >&2 <<EOF
+
+    $prog ${FUNCNAME##h_} log|cold|warm|session
+
+Some shortcuts for using the Emulator emu.mk
+
+Log perfoms a tail on the latest appium logfile and only works if you have the full m_.sh 
+implementations.
+
+Only make -f emu.mk clean if you want to delete all the log files.
+
+EOF
+
 }
 
 d_mk () {
-  test $# -ge 1 || return 1
-  local cmd="$1"
-  shift
+    test $# -ge 1 || return 1
+    local cmd="$1"
+    shift
 
-  case $cmd in
-    log)
-      $nodo tail -f $(m_ -d . mr 'appium-*.log')
-      ;;
-    cold)
-      make ${nodo:+"-n"} -f emu.mk clean-local
-      make ${nodo:+"-n"} -f emu.mk X_SNAPSHOT=-no-snapshot-load all-local
-      ;;
-    warm)
-      make ${nodo:+"-n"} -f emu.mk clean-local
-      make ${nodo:+"-n"} -f emu.mk all-local
-      ;;
-    session)
-      make ${nodo:+"-n"} session-live
-      ;;
-  esac  
+    local fmk=emu.mk
+    test -f $fmk || return 2
+
+    case $cmd in
+	log)
+	    $nodo tail -f $($prog -d . mr 'appium-*.log')
+	    ;;
+	cold)
+	    make ${nodo:+"-n"} -f $fmk clean-local
+	    make ${nodo:+"-n"} -f $fmk X_SNAPSHOT=-no-snapshot-load all-local
+	    ;;
+	warm)
+	    make ${nodo:+"-n"} -f $fmk clean-local
+	    make ${nodo:+"-n"} -f $fmk all-local
+	    ;;
+	session)
+	    make ${nodo:+"-n"} session-live
+	    ;;
+    esac  
 }
 
 h_aa () {
-  cat >&2 <<EOF
+    cat >&2 <<EOF
+  Android Packaging Utility
 
     $prog [-f badging.lst] ${FUNCNAME##h_} badging|package|activity
 
@@ -309,31 +391,164 @@ EOF
 }
 
 d_aa () {
-  test $# -ge 1 || return 1
-  local cmd="$1"
-  shift
+    test $# -ge 1 || return 1
+    local cmd="$1"
+    shift
 
-  : ${d_dir:=.}
-  : ${d_file:=${d_dir}/badging.lst}
+    : ${d_dir:=.}
+    : ${d_file:=${d_dir}/badging.lst}
 
-  case $cmd in
-    badging)
-      test $# -ge 1 || return 4
+    case $cmd in
+	badging)
+	    test $# -ge 1 || return 4
 
-      local tfile=$(mktemp)
-      f_tpush $tfile
-      aapt dump badging "$1" > $d_file
-      ;;
-    package)
-      test -f ${d_file} || return 2
-      awk -F" " '/package/ {print $2}' $d_file | awk -F"'" '/name=/ {print $2}'
-      ;;
-    activity)
-      test -f ${d_file} || return 2
-      awk -F" " '/launchable-activity/ {print $2}' $d_file | awk -F"'" '/name=/ {print $2}'
-      ;;
-    start)
-      echo shell am start -n $($FUNCNAME package)/$($FUNCNAME activity)
-      ;;
-  esac
+	    local tfile=$(mktemp)
+	    f_tpush $tfile
+	    aapt dump badging "$1" > $d_file
+	    ;;
+	package)
+	    test -f ${d_file} || return 2
+	    awk -F" " '/package/ {print $2}' $d_file | awk -F"'" '/name=/ {print $2}'
+	    ;;
+	activity)
+	    test -f ${d_file} || return 2
+	    awk -F" " '/launchable-activity/ {print $2}' $d_file | awk -F"'" '/name=/ {print $2}'
+	    ;;
+	start)
+	    echo shell am start -n $($FUNCNAME package)/$($FUNCNAME activity)
+	    ;;
+    esac
+}
+
+d_conv () {
+    test $# -ge 1 || return 1
+    local cmd="$1"
+    shift
+
+    : ${d_dir:=.}
+
+    case $cmd in
+	shell)
+	    # Convert a properties file to a series of shell set commands.
+	    # There's a trim() method in there, comments are ignored, and in the
+	    # properties key . is replaced with _. The value is quoted.
+	    $nodo awk -F= 'BEGIN { FS="="; OFS="=" } 
+function trim(f, p) { p=f; gsub(/^[ ]*/, "", p); gsub(/[ ]+$/, "", p); return p }
+/^#/ { next }
+NF >= 2 { $1=trim($1); gsub(/\./, "_", $1); $2=trim($2); $2="\""$2"\""; print }'
+	    ;;
+	
+	appium)
+	    test $# -ge 1 || return 1
+	    d_file="$1"
+	    test -f "${d_file}" || return 2
+
+	    # Takes a testconfig.properties and converts it to a JSON to start an Appium session with
+	    # see $prog app session-create
+	    : ${d_log:=$(mktemp)}
+
+	    local tfile=$(mktemp)
+	    f_tpush $tfile
+
+	    # convert to a shell script, source that and then use a in-place file
+	    cat $d_file | $FUNCNAME shell > $tfile
+	    source $tfile
+
+	    cat > $d_log <<EOF
+{
+    "desiredCapabilities": {
+        "app":  "$android_capability_app_o2",
+        "deviceName": "$capability_deviceName",
+        "appActivity": "$android_capability_appActivity",
+        "appPackage": "canvasm.myo2",
+        "newCommandTimeout": $capability_newCommandTimeout,
+	"noReset": $capability_noReset,
+	"fullReset": $android_capability_fullReset,
+        "platformVersion": "$capability_platformVersion",
+        "automationName": "$capability_automationName",
+        "platformName": "$capability_platformName",
+        "avd": "$android_capability_avd"
+    },
+    "capabilities": {
+        "firstMatch": [
+            {
+                "appium:app": "$android_capability_app_o2",
+                "appium:appActivity": "$android_capability_appActivity",
+                "appium:appPackage": "canvasm.myo2",
+                "appium:automationName":  "$capability_automationName",
+                "appium:avd":  "$android_capability_avd"
+                "appium:deviceName":  "$capability_deviceName",
+                "appium:newCommandTimeout": $capability_newCommandTimeout,
+		"noReset": $capability_noReset,
+		"fullReset": $android_capability_fullReset,
+                "platformName": "$capability_platformName",
+                "appium:platformVersion": "$capability_platformVersion"
+            }
+        ]
+    }
+}
+
+EOF
+	    ;;
+	
+	package)
+	    true
+	    ;;
+    esac
+}
+
+## Some basic XML
+
+d_xml () {
+    test $# -ge 1 || return 1
+    local cmd="$1"
+    shift
+    local xml=xmlstarlet
+
+    : ${d_dir:=pages}
+    : ${d_file:=$($prog -d "$d_dir" mr 'w*')}
+
+    case $cmd in
+	structure)
+	    $nodo $xml sel -T -t -m '//*' -m 'ancestor-or-self::*' -v 'name()' -i 'not(position()=last())' -o . -b -b -n $d_file
+	    ;;
+	textview)
+	    $nodo $xml sel -t -c "/hierarchy//*/android.widget.TextView" -n $d_file
+	    ;;
+	clickable-radiobutton)
+	    $nodo $xml sel -t -c "/hierarchy//*[android.widget.RadioButton/@clickable = 'true']" -n $d_file
+	    ;;
+	clickable-edittext)
+	    $nodo $xml sel -t -c "/hierarchy//*[android.widget.EditText/@clickable = 'true']" -n $d_file
+	    ;;
+	clickable)
+	    $nodo $xml sel -t -c "/hierarchy//*[*/@clickable = 'true']" -n $d_file
+	    ;;
+	text-nonempty)
+	    $nodo $xml ed -d "/hierarchy//*[*/@text = '']" $d_file
+	    ;;
+	text-empty)
+	    $nodo $xml sel -t -c "/hierarchy//*[*/@text = '']" -n $d_file
+	    ;;
+	cmdline)
+	    # "/hierarchy/@index" => all hierarchy 0
+	    # "//@resource-id" 
+	    $nodo xmlstarlet sel -T -t -v $* $d_file
+	    ;;
+	all)
+	    test -n "$d_service" || return 4
+	    test -f "$d_service" || return 2
+	    
+	    for d_file in $(cat $d_service | xargs)
+	    do
+		echo -- $d_file
+		$FUNCNAME $d_service $*
+	    done
+	    ;;
+	*)
+	    $nodo $xml tr ${cmd}.xsl*  $d_file
+	    ;;
+	    
+    esac
+    
 }
